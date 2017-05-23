e00f5b177525851837e485860782d8aca35a2f8f
Naveenkumar6969 <sspoweravenue@gmail.com>
2017-05-23 12:20:07 +0530

    Commit 12
    Adding admin logins

--- src/main/java/com/epam/command/CommandProvider.java
+++ src/main/java/com/epam/command/CommandProvider.java
@@ -8,6 +8,7 @@ import com.epam.command.impl.ChangeLanguageCommand;
 import com.epam.command.impl.LoginCommand;
 import com.epam.command.impl.LogoutCommand;
 import com.epam.command.impl.SearchCommand;
+import com.epam.command.impl.admin.DashBoardCommand;
 import com.epam.command.impl.user.DisplayBooksCommand;
 import com.epam.command.impl.user.FavouritesCommand;
 import com.epam.command.impl.user.NewUserCommand;
@@ -17,6 +18,7 @@ public class CommandProvider {
 	private static Map<String, Command> commandMap = new HashMap<String, Command>();
 	
 	public CommandProvider(){
+		commandMap.put("admin", new DashBoardCommand());
 		commandMap.put("login", new LoginCommand());
 		commandMap.put("logout", new LogoutCommand());
 		commandMap.put("new", new NewUserCommand());
@@ -24,8 +26,8 @@ public class CommandProvider {
 		commandMap.put("book", new BookCommand());
 		commandMap.put("search", new SearchCommand());
 		commandMap.put("changelanguage", new ChangeLanguageCommand());
-		commandMap.put(null, new DisplayBooksCommand());
 		commandMap.put("favourites", new FavouritesCommand());
+		commandMap.put(null, new DisplayBooksCommand());
 		/*commandMap.put("get", new PageLoadCommand());
 		commandMap.put("login", new LoginCommand());
 		
